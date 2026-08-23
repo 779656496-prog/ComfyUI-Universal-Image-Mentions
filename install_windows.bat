a7942cdb0400fc2de0d637a814faa2379f5c2dff
779656496-prog <779656496@qq.com>
2026-08-23 23:01:53 +0800

    Add install_windows.bat

---
+++ install_windows.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set "HERE=%~dp0"
+echo Copy the folder ComfyUI-Universal-Image-Mentions into your ComfyUI\custom_nodes\ directory.
+echo No pip install is required.
+echo Then restart ComfyUI.
+pause
